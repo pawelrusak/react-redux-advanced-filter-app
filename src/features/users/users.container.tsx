cc4ccb3be6278900af6a685b55d804cfd062b5fb
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-18 15:30:33 +0200

    feat: display data of users from API in table

--- src/features/users/users.container.tsx
+++ src/features/users/users.container.tsx
@@ -1,9 +1,16 @@
+import { useGetUsersQuery } from "./users.api";
+import { UsersTable } from "@/features/users/components";
+
 export function UsersContainer() {
+  const { data: users } = useGetUsersQuery();
+
   return (
     <main>
       <header>
         <h1>Users</h1>
       </header>
+
+      <UsersTable users={users ?? []} />
     </main>
   );
 }
