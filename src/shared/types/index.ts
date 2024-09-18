3e1f69edc70fe01c2869b1d6aa2f2acfff50633c
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-18 14:05:13 +0200

    chore: create shared types

---
+++ src/shared/types/index.ts
@@ -0,0 +1,1 @@
+export type { Address, Company, Id } from "@/shared/types/shared.types";
---
+++ src/shared/types/shared.types.ts
@@ -0,0 +1,29 @@
+type Id = {
+  id: string;
+};
+
+type Geolocation = {
+  lat: string;
+  lng: string;
+};
+
+type AddressBase = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+};
+
+type AddressGeolocation = {
+  geo: Geolocation;
+};
+
+type Address = AddressBase & AddressGeolocation;
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+export type { Id, Address, Company };
