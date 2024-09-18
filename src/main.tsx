d83b6892768c55e62b77df8d38bbe8727f3b791e
pawelrusak <78496242+pawelrusak@users.noreply.github.com>
2024-09-18 15:27:45 +0200

    chore: setup Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store.ts";
-import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import { store } from "@/store.ts";
+import App from "@/App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+    </Provider>
+  </StrictMode>
+);
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
